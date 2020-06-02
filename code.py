ae7a9912e3dafec99c9431824718932f58d69916
mahnitin <66030105+mahnitin@users.noreply.github.com>
2020-06-02 19:49:51 +0545

    code.py

---
+++ code.py
@@ -0,0 +1,42 @@
+import random
+roll="y"
+while roll=="y":
+    i=random.randint(1,6)
+    if i==1:
+        print("..........")
+        print(".        .")
+        print(".   0    .")
+        print(".        .")
+        print("..........")
+    if i==2:
+        print("..........")
+        print(".        .")
+        print(". 0    0 .")
+        print(".        .")
+        print("..........")
+    if i==3:
+        print("..........")
+        print(".   0    .")
+        print(".   0    .")
+        print(".   0    .")
+        print("..........")
+    if i==4:
+        print("..........")
+        print(". 0   0  .")
+        print(".        .")
+        print(". 0   0  .")
+        print("..........")
+    if i==5:
+        print("..........")
+        print(". 0   0  .")
+        print(".   0    .")
+        print(". 0   0  .")
+        print("..........")
+    if i==6:
+        print("..........")
+        print(". 0    0 .")
+        print(". 0    0 .")
+        print(". 0    0 .")
+        print("..........")    
+
+    roll=input( "enter y to roll again :\n")
